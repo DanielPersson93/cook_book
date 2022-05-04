62444949c1e28579481bf657abe7946ba990dc30
danielpersson93 <daniel.persson93@hotmail.se>
2022-05-04 18:12:47 +0100

    added hover effect on ButtonComponent and CategoryComponent

--- client-cook-book/src/components/componentsHome/SectionLeft/CategoryList.tsx
+++ client-cook-book/src/components/componentsHome/SectionLeft/CategoryList.tsx
@@ -9,6 +9,16 @@ font-size: 20px;
 list-style:none;
 text-align:left;
 padding-top:0.5rem;
+transition: ease-in 0.5s;
+&:hover{
+    color:#16a300;
+    /* transform:scale(1.1); */
+    transition: ease-in 0.5s;
+}
+&:active{
+    color:#16a300;
+
+}
 `
 
 export default CategoryList
--- client-cook-book/src/components/componentsHome/SectionRight/componentsRecipe/ReadMoreButton.tsx
+++ client-cook-book/src/components/componentsHome/SectionRight/componentsRecipe/ReadMoreButton.tsx
@@ -18,6 +18,16 @@ font-size: 20px;
 line-height: 24px;
 text-align:center;
 align-self:center;
+border:none;
+transition: ease-in 0.5s;
+
+
+&:hover{
+    color:black;
+    background: #9AFF89;
+    transform:scale(1.1);
+    transition: ease-in 0.5s;
+}
 `
 
 
