9fbb3af782e8eaf9e175802a2319355b1cb83d23
danielpersson93 <daniel.persson93@hotmail.se>
2022-05-20 10:04:51 +0100

    added script Single.tsx

--- client-cook-book/src/pages/single/SinglePage.tsx
+++ client-cook-book/src/pages/single/SinglePage.tsx
@@ -36,6 +36,7 @@ const SinglePage = () => {
             setRecipe(recipe);
             dispatch({ payload: recipe.ratings, type: ReviewActionType.SetReviews });
         })
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [id])
 
     return (
