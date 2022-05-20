884350ff488eaac0781154a8f34e242ea8cd5b93
danielpersson93 <daniel.persson93@hotmail.se>
2022-05-20 10:20:45 +0100

    added .env in client and added alternative host name

--- client-cook-book/src/services/axios.service.tsx
+++ client-cook-book/src/services/axios.service.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'http://localhost:4000',
+    baseURL: 'http://localhost:4000' || 'https://server-cook-book.herokuapp.com/',
     responseType: 'json'
 });
 
