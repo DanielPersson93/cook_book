7baa7b5a159d77ae2b8a869ccb600691261d8068
danielpersson93 <daniel.persson93@hotmail.se>
2022-05-20 10:27:37 +0100

    changed url

--- client-cook-book/src/services/axios.service.tsx
+++ client-cook-book/src/services/axios.service.tsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const httpClient = axios.create({
-    baseURL: 'https://server-cook-book.herokuapp.com',
+    baseURL: 'https://server-cook-book.herokuapp.com/',
     responseType: 'json'
 });
 
